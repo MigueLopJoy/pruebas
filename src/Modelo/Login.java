87671773482824e76e7e7ec16e6a1cf699933acc
= <m.lop.rod.97@gmail.com>
2023-05-16 17:48:48 +0200

    Estructura basica de las clases del modelo

--- src/Modelo/Login.java
+++ src/Modelo/Login.java
@@ -1,4 +1,42 @@
 package Modelo;
 
 public class Login {
+    private int idLogin;
+
+    private String nombreUsuario;
+    private String contraseña;
+
+    public Login() {
+        super();
+    }
+
+    public Login(String nombreUsuario, String contraseña) {
+        super();
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public int getIdLogin() {
+        return idLogin;
+    }
+
+    public void setIdLogin(int idLogin) {
+        this.idLogin = idLogin;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 }
--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -1,4 +1,46 @@
 package Modelo;
 
 public class Usuario {
+
+    private int idUsuario;
+    private String nombre;
+
+    private Login login;
+    public Usuario() {
+        super();
+    }
+
+    public Usuario(String nombre, Login login) {
+        super();
+        this.nombre = nombre;
+        this.login = login;
+    }
+    public Usuario(int idUsuario, String nombre, Login login) {
+        super();
+        this.idUsuario = idUsuario;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
 }
