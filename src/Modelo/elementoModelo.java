d8426d6e3168c633ae49c1a6ea1222c82184ee76
= <m.lop.rod.97@gmail.com>
2023-05-16 18:23:58 +0200

    eliminacion de metodo update de interfaz 'elementoModelo'

--- src/Modelo/elementoModelo.java
+++ src/Modelo/elementoModelo.java
@@ -3,9 +3,7 @@ package Modelo;
 public interface elementoModelo {
 
     public String getInsertString();
-
-    public String getUpdateString();
-
+    
     public String getDeleteString();
 
     public int getIdFromDB();
