3631eeddae17c0457a0aacc02fbee3572aa2e2b1
= <m.lop.rod.97@gmail.com>
2023-05-16 17:42:13 +0200

    Creada clase de conexion a la clase BDD

--- src/DataHandler/DBConnection.java
+++ src/DataHandler/DBConnection.java
@@ -1,4 +1,31 @@
 package DataHandler;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+
+    private String className;
+    private String url;
+    private Connection connection;
+
+    public DBConnection(String user, String password) {
+        this.className = "com.mysql.cj.jdbc.Driver";
+        this.url = "jdbc:mysql://localhost:3307/conexiones";
+        this.connection = null;
+
+        try {
+            Class.forName(className);
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
 }
